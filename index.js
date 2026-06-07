30145d271b01d062cea55f3f9bbdbf316536e758
zelgo-azm <zelgo@azmklan.org>
2026-06-07 19:12:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+const TOKEN = '';
+const CHANNEL_ID = '1315633892266348606';
+
+client.once('ready', () => {
+    console.log(`Bot uruchomiony jako ${client.user.tag}`);
+
+    client.user.setActivity('Władca Grzechu');
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.get(CHANNEL_ID);
+
+    if (channel) {
+        channel.send(
+            `🔥 Witaj ${member} jesteś ${member.guild.memberCount} duszą w Czeluści Klanu AZM! 🔥`
+        );
+    }
+});
+
+client.login(TOKEN);
+
