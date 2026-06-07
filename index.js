9916758d121c0e5d1d7b5b2647dfc49b3837b35e
zelgo-azm <zelgo@azmklan.org>
2026-06-07 23:37:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server działa");
+});
+
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
 });
 
-const CHANNEL_ID = '1315633892266348606';
+const CHANNEL_ID = "1315633892266348606";
 
-client.once('ready', () => {
-    console.log(`Bot uruchomiony jako ${client.user.tag}`);
+client.once("ready", () => {
+  console.log(`Bot uruchomiony jako ${client.user.tag}`);
 
-    client.user.setActivity('Włada Grzechem', {
-        type: ActivityType.Playing
-    });
+  client.user.setActivity("Włada Grzechem", {
+    type: ActivityType.Playing
+  });
 });
 
-client.on('guildMemberAdd', async (member) => {
-    const channel = await member.guild.channels.fetch(CHANNEL_ID).catch(() => null);
-    if (!channel) return;
+client.on("guildMemberAdd", async (member) => {
+  const channel = await member.guild.channels.fetch(CHANNEL_ID).catch(() => null);
+  if (!channel) return;
 
-    channel.send(
-        `🔥 Witaj ${member} 👋 Jesteś ${member.guild.memberCount} potępionym w Czeluści Klanu AZM! 🔥`
-    );
+  channel.send(
+    `🔥 Witaj ${member} 👋 Jesteś ${member.guild.memberCount} potępionym w Czeluści Klanu AZM! 🔥`
+  );
 });
 
 client.login(process.env.TOKEN);
