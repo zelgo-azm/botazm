8c7cccfcea7e48f0a32362468fb7ca72891a4eb9
zelgo-azm <zelgo@azmklan.org>
2026-06-07 23:55:42 +0200

    Integrate Express web server into the application
    Added an Express web server that responds with 'OK'.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("OK");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Web server działa");
+});
+
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
