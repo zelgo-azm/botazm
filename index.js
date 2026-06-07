002adfa47d3a668d8d500f31abf8aa1a03c4e1d6
zelgo-azm <zelgo@azmklan.org>
2026-06-07 20:24:42 +0200

    Refactor bot to use environment variable for token
    Updated bot activity type and removed hardcoded token.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-console.log("🔥 BOT FILE STARTED");
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -9,13 +7,14 @@ const client = new Client({
     ]
 });
 
-const TOKEN = 'MTExNTM1Nzc5OTU1NjMyOTUyNQ.GR0adA.3ywyVWbXlVE9doL8SNLeptEMrDIlS3n-Usnx0E';
 const CHANNEL_ID = '1315633892266348606';
 
 client.once('ready', () => {
     console.log(`Bot uruchomiony jako ${client.user.tag}`);
 
-    client.user.setActivity('Władca Grzechu');
+    client.user.setActivity('Władca Grzechu', {
+        type: ActivityType.Playing
+    });
 });
 
 client.on('guildMemberAdd', member => {
@@ -28,5 +27,5 @@ client.on('guildMemberAdd', member => {
     }
 });
 
-client.login(TOKEN);
-
+// 🔥 TOKEN Z ENV (Render / .env lokalnie)
+client.login(process.env.TOKEN);
